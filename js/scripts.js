5b76bf6164131fe49290ab210a2e3823dbd3836a
Cindy Ward <cindyward@yahoo.com>
2015-09-19 22:34:29 -0700

    implement third test case

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,16 @@ var bottlesOfBeer = function(numberBottles, originalNumber) {
   var sentence = "";
   var bottles = "bottles";
   var bottlesMinusOne = "";
-  if (numberBottles > 2) {
-    bottlesMinusOne = (index - 1) + " bottles"
-  } else if (numberBottles == 2) {
-    bottlesMinusOne = "1 bottle"
-  } else if (numberBottles == 1){
-    bottles = "bottle";
-    bottlesMinusOne = "No more bottles";
+  switch(numberBottles) {
+    case 1:
+      bottles = "bottle";
+      bottlesMinusOne = "No more bottles";
+      break;
+    case 2:
+      bottlesMinusOne = "1 bottle";
+      break;
+    default:
+      bottlesMinusOne = (numberBottles - 1) + " bottles";
   };
   if (numberBottles > 0) {
     sentence = "<li>" + numberBottles + " " + bottles + " of beer on the wall, " + numberBottles + " " + bottles + " of beer. Take one down and pass it around, " + bottlesMinusOne + " of beer on the wall.</li>" + (bottlesOfBeer((numberBottles - 1), originalNumber));
